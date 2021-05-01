515370719a1a93e07447c40f32c256c15631b590
Denisse Ogaz <dvogaz@uc.cl>
2021-05-01 13:03:35 -0400

    wasm

--- main.cpp
+++ main.cpp
@@ -1,64 +1,90 @@
 // CPP program to implement traveling salesman
 // problem using naive approach.
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <cstdint>
+#include <cstddef>
+#include <iostream>
+#include <cstring>
+#include<vector>
+
+#include <emscripten.h>
 using namespace std;
-#define V 4
  
 // implementation of traveling Salesman Problem
-int travllingSalesmanProblem(int graph[][V], int s)
+uint32_t travllingSalesmanProblem(uint32_t** graph, int*route, uint32_t s, uint32_t min_path, uint32_t V)
 {
     // store all vertex apart from source vertex
     vector<int> vertex;
-    for (int i = 0; i < V; i++)
+    for (uint32_t i = 0; i < V; i++)
         if (i != s)
             vertex.push_back(i);
  
     // store minimum weight Hamiltonian Cycle.
-    int min_path = INT_MAX;
-    int route[10];
-    int count = 1;
-    route[0] = s;
     do {
-        printf("%d\n", vertex[0]);
         // store current Path weight(cost)
-        int current_pathweight = 0;
+        uint32_t current_pathweight = 0;
  
         // compute current path weight
-        int k = s;
-        for (int i = 0; i < vertex.size(); i++) {
-            current_pathweight += graph[k][vertex[i]];
+        uint32_t k = s;
+        for (uint32_t i = 0; i < vertex.size(); i++) {
+            if (graph[k][vertex[i]] == -1)
+            {
+                current_pathweight = INT32_MAX;
+            } else if (current_pathweight < INT32_MAX){
+                current_pathweight += graph[k][vertex[i]];
+            }
             k = vertex[i];
         }
-        current_pathweight += graph[k][s];
  
         // update minimum
         min_path = min(min_path, current_pathweight);
         if (min_path == current_pathweight) {
-            route[count] = k;
-            count +=1;
+            route[0] = s;
+            for (uint32_t i = 1; i < V; i++)
+            {
+                route[i] = vertex[i-1];
+            }
         }
  
     } while (
         next_permutation(vertex.begin(), vertex.end()));
     
-    for (int i = 0; i < 10; i++)
-    {
-        // printf("%d\n", route[i]);
-    }
-    
  
     return min_path;
 }
- 
-// Driver Code
-int main()
-{
-    // matrix representation of graph
-    int graph[][V] = { { 0, 10, 15, 20 },
-                       { 10, 0, 35, 25 },
-                       { 15, 35, 0, 30 },
-                       { 20, 25, 30, 0 } };
-    int s = 0;
-    cout << travllingSalesmanProblem(graph, s) << endl;
-    return 0;
+
+extern "C" {
+    uint32_t findbest(uint32_t** graph, int*route, uint32_t V){
+    uint32_t min_path = INT_MAX;
+    for (uint32_t s = 0; s < V; s++)
+    {
+        min_path = travllingSalesmanProblem(graph, route, s, min_path, V);
+        // printf("Min-path: %i - Ruta:", min_path);
+        // for (uint32_t i = 0; i < V; i++)
+        // {
+        //     printf("%c", lista[route[i]]);
+        // }
+        // printf("\n");
+    }
+    return min_path;
+    }
 }
+ 
+// // Driver Code
+// int main()
+// {
+//     // matrix representation of graph
+//     int graph[][7] = { { 0 , 10,-1, 8 , 7 ,-1, -1 }, //A
+//                        { 10, 0 , 12, 7 , -1, -1, -1 }, //B
+//                        { -1, 12, 0 , 6 , -1, 7 ,  5 }, //C
+//                        { 8 , 7 , 6 , 0 , 9 , 4 , -1 }, //d
+//                        { 7 , -1, -1, 9 , 0 , -1, 11 },// E
+//                        { -1, -1, 7 , 4 , -1, 0 ,  3 }, //F
+//                        { -1, -1, 5 , -1, 11, 3 ,  0 } //G
+//                     };
+                
+//     int* route = (int *)calloc(V, sizeof(int));
+//     cout << findbest(graph, route) << endl;
+//     free(route);
+//     return 0;
+// }
