3005ae55c9ede4f40829af212c64318c9ae76eb8
Macarena Concha <maconcha4@uc.cl>
2021-05-01 23:40:42 -0400

    obtener ruta más corta

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ var options = {
 /* Variables Función */
 const matrix = (rows, cols) => new Array(cols).fill(-1).map((o, i) => new Array(rows).fill(-1));
 var graph_matrix = null;
+var route = [];
 
 /* Obtener la cantidad de nodos */
 const get_nodes = () => {
@@ -50,37 +51,3 @@ const add_conexion = () => {
 };
 
 
-/* Función que genera un puntero a partir de la matriz */
-const make_ptr = (myModule) => {
-  const arrayPtr = myModule._calloc(parseInt(n_nodes), 4);
-  for (let i = 0; i < n_nodes; i++) {
-    let rowsPtr = myModule._calloc(parseInt(n_nodes), 4);
-    myModule.setValue(arrayPtr + i * 4, rowsPtr, "i32");
-    for (let j = 0; j < n_nodes; j++) {;
-      myModule.setValue(rowsPtr + j * 4, graph_matrix[i][j], "i32");
-    }
-  }
-  return arrayPtr;
-};
-
-/* Función que genera una matriz a partir de un puntero */
-const to_string = (myModule, ptr) => {
-  let result = myModule.getValue(ptr + 4, "i32");
-  return result;
-};
-
-/* Obtener la ruta más corta */
-
-Module().then(function(mymod){
-  let find_best_btn = document.getElementById('find_best');
-  find_best_btn.onclick = () => {
-    console.log("Hice click, ayuda");
-    let ptr = make_ptr(mymod);
-    let startTime = window.performance.now();
-    let findBestResult = mymod._findBest(ptr);
-    let endTime = window.performance.now();
-    let resultPath = to_string(mymod, ptr);
-    console.log(resultPath);
-    // Función que me cambia la ruta 
-  }
-})
