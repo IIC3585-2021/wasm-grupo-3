0318d12b3bf965241ed069ec3d5d8a54377b828d
Denisse Ogaz <dvogaz@uc.cl>
2021-05-02 00:24:48 -0400

    findbest funciona

--- find_best.js
+++ find_best.js
@@ -1,61 +1,63 @@
 import Module from "./main.js";
 
-console.log(" ayuda");
+console.log(" ayuda", n_nodes.value);
 
 /* Función que genera un puntero a partir de la matriz */
 const matrixToPtr = (myModule) => {
     const arrayPtr = myModule._calloc(parseInt(n_nodes), 4);
     for (let i = 0; i < n_nodes; i++) {
-      let rowsPtr = myModule._calloc(parseInt(n_nodes), 4);
-      myModule.setValue(arrayPtr + i * 4, rowsPtr, "i32");
-      for (let j = 0; j < n_nodes; j++) {;
-        myModule.setValue(rowsPtr + j * 4, graph_matrix[i][j], "i32");
-      }
+        let rowsPtr = myModule._calloc(parseInt(n_nodes), 4);
+        myModule.setValue(arrayPtr + i * 4, rowsPtr, "i32");
+        for (let j = 0; j < n_nodes; j++) {;
+            myModule.setValue(rowsPtr + j * 4, graph_matrix[i][j], "i32");
+        }
     }
     console.log(arrayPtr);
     return arrayPtr;
-  };
-  
-  
-  /* Función que genera un puntero a partir de la matriz */
-  const arrayToPtr = (myModule) => {
+};
+
+
+/* Función que genera un puntero a partir de la matriz */
+const arrayToPtr = (myModule) => {
     let arrayPtr = myModule._calloc(parseInt(n_nodes), 4);
     return arrayPtr;
-  };
-  
-  /* Función que genera una matriz a partir de un puntero */
-  const ptrToInt = (myModule, ptr) => {
+};
+
+/* Función que genera una matriz a partir de un puntero */
+const ptrToInt = (myModule, ptr) => {
     let result = myModule.getValue(ptr + 4, "i32");
     return result;
-  };
-  
-  /* Función que genera un array a partir de un puntero */
-  const ptrToArray = (myModule, ptr) => {
+};
+
+/* Función que genera un array a partir de un puntero */
+const ptrToArray = (myModule, ptr) => {
     let routeResult = [];
     for (let i = 0; i < n_nodes; i++) {
-      routeResult[i] = myModule.getValue(ptr + 4*i, "i32");
+        routeResult[i] = myModule.getValue(ptr + 4 * i, "i32");
     }
     return routeResult;
-  };
-  
-  /* Obtener la ruta más corta */
-  
-  Module().then(function(mymod){
+};
+
+/* Obtener la ruta más corta */
+
+Module().then(function(mymod) {
     let find_best_btn = document.getElementById('find_best');
     let findBestResult = 0;
     find_best_btn.onclick = () => {
-        console.log(graph_matrix)
-    console.log("Hice click, ayuda");
-    let ptrMatrix = matrixToPtr(mymod);
-    let ptrArray = arrayToPtr(mymod);
-    let startTime = window.performance.now();
-    console.log("Tiempo inicial:", startTime);
-    findBestResult = mymod._findbest(ptrMatrix, ptrArray, parseInt(n_nodes));
-    console.log("Costo: ", findBestResult);
-    let endTime = window.performance.now();
-    console.log("Tiempo final:", endTime);
-    let resultPath = ptrToArray(mymod, ptrArray);
-    console.log("Arreglo:", resultPath);
-      // Función que me cambia la ruta 
+        n_nodes = n_nodes.value;
+        console.log(graph_matrix);
+        console.log("Hice click, ayuda");
+        let ptrMatrix = matrixToPtr(mymod);
+        let ptrArray = arrayToPtr(mymod);
+        let startTime = window.performance.now();
+        console.log("Tiempo inicial:", startTime);
+        console.log("Nodos:", n_nodes);
+        findBestResult = mymod._findbest(ptrMatrix, ptrArray, parseInt(n_nodes));
+        console.log("Costo: ", findBestResult);
+        let endTime = window.performance.now();
+        console.log("Tiempo final:", endTime);
+        let resultPath = ptrToArray(mymod, ptrArray);
+        console.log("Arreglo:", resultPath);
+        // Función que me cambia la ruta 
     }
-  })
+})
--- index.js
+++ index.js
@@ -9,13 +9,13 @@ var cost = document.getElementById('value');
 var nodes = new vis.DataSet({})
 var edges = new vis.DataSet({});
 var network = null;
-var data= {
-  nodes: nodes,
-  edges: edges,
+var data = {
+    nodes: nodes,
+    edges: edges,
 };
 var options = {
-  width: '500px',
-  height: '500px',
+    width: '500px',
+    height: '500px',
 };
 
 /* Variables Función */
@@ -25,29 +25,27 @@ var route = [];
 
 /* Obtener la cantidad de nodos */
 const get_nodes = () => {
-  if (!n_nodes.value || n_nodes.value <= 1){
-    alert("Ingresa una cantidad de nodos válida");
-  } else { 
-    create_graph(n_nodes.value);
-    // Acá falta eliminar la vista de la cantidad de nodos.
-  }
+    if (!n_nodes.value || n_nodes.value <= 1) {
+        alert("Ingresa una cantidad de nodos válida");
+    } else {
+        create_graph(n_nodes.value);
+        // Acá falta eliminar la vista de la cantidad de nodos.
+    }
 };
 
 /* Creo la matriz que va a contener el grafo y el objeto que se muestra */
 const create_graph = (n_nodes) => {
-  graph_matrix = matrix(parseInt(n_nodes),parseInt(n_nodes));
-  for (let i = 0; i < n_nodes; i++) {
-    nodes.add({id: i, label: i.toString()});
-    graph_matrix[i][i] = 0;
-  };
-  network = new vis.Network(container, data, options);
+    graph_matrix = matrix(parseInt(n_nodes), parseInt(n_nodes));
+    for (let i = 0; i < n_nodes; i++) {
+        nodes.add({ id: i, label: i.toString() });
+        graph_matrix[i][i] = 0;
+    };
+    network = new vis.Network(container, data, options);
 };
 
 /* Agregar conexión al grafo */
 const add_conexion = () => {
-  edges.add({from: from.value, to:to.value, label: cost.value});
-  graph_matrix[from.value][to.value] = parseInt(cost.value);
-  graph_matrix[to.value][from.value] = parseInt(cost.value);
+    edges.add({ from: from.value, to: to.value, label: cost.value });
+    graph_matrix[from.value][to.value] = parseInt(cost.value);
+    graph_matrix[to.value][from.value] = parseInt(cost.value);
 };
-
-
