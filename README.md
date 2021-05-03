18c70b70ec2a504a609cad3aa635451708007ec8
dvogaz <dvogaz@uc.cl>
2021-05-02 23:16:43 -0400

    Html + wasm (#1)
    * .gitignore
    
    * cpp
    
    * create graph
    
    * wasm
    
    * html listo
    
    * update
    
    * obtener ruta más corta
    
    * findbest funciona
    
    * draw best route
    
    * Tarea 3 final
    
    * fix button
    
    * fix: milisegundos
    
    Co-authored-by: Macarena Concha <maconcha4@uc.cl>

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-"# wasm-grupo-3" 
+# wasm-grupo-3
+
+## Integrantes: 
+
+- Macarena Concha
+- Cristóbal Muñoz
+- Denisse Ogaz
+
+## Ejecutar el código
+
+1. `npm install`
+2. `http-server ./ -c-1`
+3. Abrir http://localhost:8080/
+
+## Fuentes: 
+
+* Dibujar Grafos: Vis.js. https://visjs.org/index.html
+
---
+++ index.js
@@ -0,0 +1,90 @@
+/* Variables HTML */
+const container = document.getElementById('graph')
+    // eslint-disable-next-line prefer-const
+let nNodes = document.getElementById('nNodes')
+const from = document.getElementById('node1')
+const to = document.getElementById('node2')
+const cost = document.getElementById('value')
+const createGraphBtn = document.getElementById('createGraphBtn')
+const addEdgeBtn = document.getElementById('addEdgeBtn')
+const graphPanel = document.getElementById('graph')
+const resultPanel = document.getElementById('resultPanel')
+const findBestBtn = document.getElementById('findBestBtn')
+
+/* Variables Grafo */
+const nodes = new vis.DataSet({})
+const edges = new vis.DataSet({})
+let network = null
+const data = {
+    nodes: nodes,
+    edges: edges
+}
+const options = {
+    width: '800px',
+    height: '400px'
+}
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+/* Variables Función */
+const matrix = (rows, cols) => new Array(cols).fill(-1).map((o, i) => new Array(rows).fill(-1))
+let graphMatrix = null
+    // eslint-disable-next-line prefer-const
+let route = []
+
+/* Obtener la cantidad de nodos */
+createGraphBtn.onclick = () => {
+    if (!nNodes.value || nNodes.value <= 1) {
+        alert('Ingresa una cantidad de nodos válida')
+    } else {
+        createGraph(nNodes.value)
+        nNodes.disabled = true
+        graphPanel.hidden = false
+        from.disabled = false
+        to.disabled = false
+        cost.disabled = false
+        addEdgeBtn.disabled = false
+        findBestBtn.disabled = false
+    }
+}
+
+/* Creo la matriz que va a contener el grafo y el objeto que se muestra */
+const createGraph = (nNodes) => {
+    graphMatrix = matrix(parseInt(nNodes), parseInt(nNodes))
+    for (let i = 0; i < nNodes; i++) {
+        nodes.add({ id: i, label: alphabet[i] })
+        graphMatrix[i][i] = 0
+    };
+    network = new vis.Network(container, data, options)
+}
+
+/* Agregar conexión al grafo */
+addEdgeBtn.onclick = () => {
+    edges.add({ from: alphabet.indexOf(from.value), to: alphabet.indexOf(to.value), label: cost.value })
+    graphMatrix[alphabet.indexOf(from.value)][alphabet.indexOf(to.value)] = parseInt(cost.value)
+    graphMatrix[alphabet.indexOf(to.value)][alphabet.indexOf(from.value)] = parseInt(cost.value)
+}
+
+/* Dibujar la ruta más corta */
+const getBestRoute = (value, startTime, endTime) => {
+    let edge;
+    let direction;
+    let routeString = ''
+    resultPanel.hidden = false
+    from.disabled = true
+    to.disabled = true
+    cost.disabled = true
+    if (value === 2147483647) {
+        alert('No hay ruta que recorra todos los nodos :(')
+    } else {
+        for (let index = 0; index < route.length - 1; index++) {
+            edge = edges.get({ filter: (item) => { return (item.from === route[index + 1] && item.to === route[index]) || (item.from === route[index] && item.to === route[index + 1]) } })[0]
+            direction = edge.from === route[index] ? 'to' : 'from'
+            edges.update({ id: edge.id, label: edge.label, from: edge.from, to: edge.to, arrows: direction, color: { color: 'green' }, width: 3, font: { size: 20 } })
+            routeString += alphabet[route[index]] + ' - '
+            if (index === route.length - 2) { routeString += alphabet[route[index + 1]] }
+        }
+        document.getElementById('bestRoute').innerHTML = 'Ruta más corta: ' + routeString
+        document.getElementById('costRoute').innerHTML = 'Costo ruta: ' + value
+        document.getElementById('time').innerHTML = 'Tiempo de ejecución: ' + Math.round((endTime - startTime) * 100000) / 100000 + ' milisegundos'
+    }
+}
